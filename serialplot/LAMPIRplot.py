75b8e21be902b9aebd6c07d1d34711feec019ae6
Libo Wu <wlbustc@gmail.com>
2018-01-29 16:21:09 -0500

    Initial commit

---
+++ serialplot/LAMPIRplot.py
@@ -0,0 +1,98 @@
+#! python3
+
+import sys
+import glob
+import serial
+from serial import SerialException
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+def serial_port():
+	'''list port names'''
+
+	if sys.platform.startswith('win'):
+		ports=['COM%s' % (i+1)for i in range (256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		ports=glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports=glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
+		
+	result=[]
+	for port in ports:
+		try:
+			s=serial.Serial(port)
+			s.close()
+			result.append(port)
+		except(OSError, serial.SerialException):
+			pass
+	return result
+	
+port_list=serial_port()
+port=''.join(port_list)
+ser=serial.Serial(port, 38400)
+
+plt.ion()
+
+start_time=time()
+timepoints=[]
+ydata=[]
+yrange=[-0.1, 5.1]
+view_time =20 #seconds of data to  view at once
+duration = 60 # total seconds to collect data
+
+fig1=plt.figure()
+fig1.suptitle('LAMPIR voltage', fontsize='18', fontweight='bold')
+plt.xlabel('time')
+plt.ylabel('voltage')
+plt.axes().grid(True)
+line1, = plt.plot(ydata)
+plt.ylim(yrange)
+plt.xlim([0,view_time])
+
+ser.flushInput()
+
+run=True
+
+while run:
+	ser.reset_input_buffer()
+	data=ser.readline()
+	
+	try:
+		ydata.append(float(data)*5.0/1024)
+		timepoints.append(time()-start_time)
+		current_time=timepoints[-1]
+		
+		line1.set_xdata(timepoints)
+		lin1.set_ydata(ydata)
+		
+		# slide the xaxis
+		if current_time>view_time:
+			plt.xlim([current_time-view_time, current_time])
+		
+		#when time is up, kill the plot and collect loop
+		if timepoints[-1]>duration: run=False
+		
+	except: pass
+	
+	fig1.canvas.draw()
+	
+ser.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ serialplot/LAMPIRplot1.py
@@ -0,0 +1,124 @@
+#! python3
+
+import sys
+import glob
+import serial
+from serial import SerialException
+import numpy as np
+import matplotlib.pyplot as plt
+from time import time
+
+def serial_port():
+	'''list port names'''
+
+	if sys.platform.startswith('win'):
+		ports=['COM%s' % (i+1)for i in range (256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		ports=glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports=glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
+		
+	result=[]
+	for port in ports:
+		try:
+			s=serial.Serial(port)
+			s.close()
+			result.append(port)
+		except(OSError, serial.SerialException):
+			pass
+	return result
+	
+port_list=serial_port()
+port=''.join(port_list)
+ser=serial.Serial(port, 38400)
+
+
+# class that holds analog data for N samples
+class AnalogData:
+  # constr
+  def __init__(self, maxLen):
+    self.ax = deque([0.0]*maxLen)
+    self.ay = deque([0.0]*maxLen)
+    self.maxLen = maxLen
+
+  # ring buffer
+  def addToBuf(self, buf, val):
+    if len(buf) < self.maxLen:
+      buf.append(val)
+    else:
+      buf.pop()
+      buf.appendleft(val)
+
+  # add data
+  def add(self, data):
+    assert(len(data) == 2)
+    self.addToBuf(self.ax, data[0])
+    self.addToBuf(self.ay, data[1])
+    
+
+# plot class
+class AnalogPlot:
+  # constr
+  def __init__(self, analogData):
+    # set plot to animated
+    plt.ion() 
+    self.axline, = plt.plot(analogData.ax)
+    self.ayline, = plt.plot(analogData.ay)
+    plt.ylim([0, 1023])
+
+  # update plot
+  def update(self, analogData):
+    self.axline.set_ydata(analogData.ax)
+    self.ayline.set_ydata(analogData.ay)
+    plt.draw()
+
+
+# main() function
+def main():
+  # expects 1 arg - serial port string
+  if(len(sys.argv) != 2):
+    print 'Example usage: python showdata.py "/dev/tty.usbmodem411"'
+    exit(1)
+
+ #strPort = '/dev/tty.usbserial-A7006Yqh'
+  strPort = sys.argv[1];
+
+  # plot parameters
+  analogData = AnalogData(100)
+  analogPlot = AnalogPlot(analogData)
+
+  print 'plotting data...'
+
+  # open serial port
+  ser = serial.Serial(strPort, 9600)
+  while True:
+    try:
+      line = ser.readline()
+      data = [float(val) for val in line.split()]
+      #print data
+      if(len(data) == 2):
+        analogData.add(data)
+        analogPlot.update(analogData)
+    except KeyboardInterrupt:
+      print 'exiting'
+      break
+  # close serial
+  ser.flush()
+  ser.close()
+
+# call main
+if __name__ == '__main__':
+  main()
+
+
+
+
+
+
+
+
+
+
+
