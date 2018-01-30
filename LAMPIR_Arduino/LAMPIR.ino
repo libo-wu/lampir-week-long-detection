c961f339e502f189d0d5a674aac55187d6929a4c
Libo Wu <wlbustc@gmail.com>
2018-01-30 18:54:46 +0000

    LAMPIR.ino edited online with Bitbucket

--- LAMPIR_Arduino/LAMPIR.ino
+++ LAMPIR_Arduino/LAMPIR.ino
@@ -10,7 +10,7 @@ Jan 2018
 #include "Filter.h"
 
 /*-----( Declare Constants, Pin Numbers )-----*/ 
-#define volNum        240   // the number of the array 
+#define volNum        400   // the number of the array 
 #define gain          1
 #define led           2
 ExponentialFilter<long> FilteredTemperature(20, 0);  //Filter is used. ExponentialFilter(weight, current)
@@ -34,7 +34,7 @@ int lavet_pulse=50;
 int lavet_delay=250;
 
 int biaVolt = 540;   // bias voltage of the ADC of Arduino
-int OccuVolDiff = 150*gain;     //  threshold voltage for occupant presence
+int OccuVolDiff = 160*gain;     //  threshold voltage for occupant presence
 int vol[volNum];
 int volRead_index=0;
 int PeakNum=0;
@@ -45,7 +45,7 @@ int MaxVol=biaVolt;
 int MinVol=biaVolt;
 int Max_index=0;
 int Min_index=0;
-byte OccuState=0; 
+byte OccuState=0x00; 
 
 
 void setup()   /*----( SETUP: RUNS ONCE )----*/
@@ -62,8 +62,8 @@ void setup()   /*----( SETUP: RUNS ONCE )----*/
     TCCR2A = 0; // set entire TCCR2A register to 0
     TCCR2B = 0; // same for TCCR2B
     TCNT2  = 0; // initialize counter value to 0
-    // set compare match register for 1000 Hz increments
-    OCR2A = 249; // = 16000000 / (64 * 1000) - 1 (must be <256)
+    // set compare match register for 500 Hz increments
+    OCR2A = 249; // f_oc2A=f_io/(2*N*(1+OCR2A))= 16000000 /(2*64*250)=500
     // turn on CTC mode
     TCCR2B |= (1 << WGM21);
     // Set CS22, CS21 and CS20 bits for 64 prescaler
@@ -104,8 +104,8 @@ ISR(TIMER2_COMPA_vect){   //update led status every n*1*volNum (ms)
        //vol[volRead_index]=(int)FilteredTemperature.Current();
        vol[volRead_index]=RawTemperature;
        
-       BTserial.print(vol[volRead_index]);    //transmission
-       BTserial.print("\n");
+//       BTserial.print(vol[volRead_index]);    //transmission
+//       BTserial.print("\n");
        
        if(vol[volRead_index]<MinVol){
         MinVol=vol[volRead_index];
@@ -125,19 +125,20 @@ ISR(TIMER2_COMPA_vect){   //update led status every n*1*volNum (ms)
 //      BTserial.print(Max_index-Min_index);
 //      BTserial.print("\n");
     if(peak2peak>=OccuVolDiff){
-      OccuState=OccuState<<1;   //if larger than threshold, then write OccuInfo[8] to 1
       bitWrite(OccuState, 0,1);
+      OccuState=OccuState<<2;   //if larger than threshold, then write OccuInfo[8] to 1   
     }
     else{
-      OccuState=OccuState<<1;   //if larger than threshold, then write OccuInfo[8] to 1
       bitWrite(OccuState, 0,0);
-    OccuState=OccuState<<2;   //if larger than threshold, then write OccuInfo[8] to 1
-      bitWrite(OccuState, 0,0);
+      OccuState=OccuState<<2;   //if larger than threshold, then write OccuInfo[8] to 1
+      
+//    OccuState=OccuState<<2;   //if larger than threshold, then write OccuInfo[8] to 1
+//      bitWrite(OccuState, 0,0);
     }
     
-    //printBits(OccuState);
+    printBits(OccuState);
     
-    if(OccuState>=0x03){
+    if(OccuState>=0x04){
       digitalWrite(led, HIGH);
     }
     else{
@@ -177,3 +178,4 @@ void backwards() {
   digitalWrite(pinB, HIGH);   
   //digitalWrite(LED_BUILTIN, HIGH);
 }
+
